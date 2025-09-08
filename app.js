1ee107f4fbda844329d352f8f06e0e23c188dd5b
Yicheng CHEN <yicheng4499@gmail.com>
2025-09-08 15:21:43 +0300

    add middleware

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const app = express();
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require('./routes/userRouter');
 
+const morgan = require('morgan');
+
+const authorizeUsersAccess = require('./middleware/auth');
+
 // Middleware to parse JSON
+app.use(morgan('tiny'));
+
 app.use(express.json());
 
 app.use("/tours", tourRouter);
 
-app.use('/users', userRouter);
+app.use('/users', authorizeUsersAccess, userRouter);
 
 const port = 4000;
 // Start the server
---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+function authorizeUsersAccess(req, res, next) {
+  if (req.query.admin === "true") {
+    req.admin = true
+    next()
+  } else {
+    res.send("ERROR: You must be an admin")
+  }
+}
+
+module.exports = authorizeUsersAccess;

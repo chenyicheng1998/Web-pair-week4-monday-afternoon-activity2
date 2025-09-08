6d68b4d657a1e47404609eea69c46fdc2a700397
Yicheng CHEN <yicheng4499@gmail.com>
2025-09-08 14:26:04 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const tourRouter = require("./routes/tourRouter");
+const userRouter = require('./routes/userRouter');
+
+// Middleware to parse JSON
+app.use(express.json());
+
+app.use("/tours", tourRouter);
+
+app.use('/users', userRouter);
+
+const port = 4000;
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
